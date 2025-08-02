f869387b767511eda0b695f24a430aae59c595aa
40davi1a <davi.franca.cavalcante@escola.pr.gov.br>
2025-08-02 19:17:02 -0300

    s

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const avaca = document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
-avaca.forEach(button=> {
-     button.addEventListener('click', function(){
-        const atual =document.querySelector('.ativo');
-        const proximoPasso= 'passo-' + this.getAttribute('data-proximo');
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
-     })
+    })
 })
